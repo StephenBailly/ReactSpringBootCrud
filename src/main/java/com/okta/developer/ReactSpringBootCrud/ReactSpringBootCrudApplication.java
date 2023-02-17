5027154e7e57df93c48cfad6912763e27b82ff6f
BAILLY <stephen.bailly@capgemini.com>
2023-02-17 10:15:35 +0100

    first commit

---
+++ src/main/java/com/okta/developer/ReactSpringBootCrud/ReactSpringBootCrudApplication.java
@@ -0,0 +1,13 @@
+package com.okta.developer.ReactSpringBootCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringBootCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringBootCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/okta/developer/ReactSpringBootCrud/ReactSpringBootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okta.developer.ReactSpringBootCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringBootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
